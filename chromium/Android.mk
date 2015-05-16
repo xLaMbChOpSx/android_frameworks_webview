ff6340dc46bccfe9e92e15517160a5f7b11d09a2
arter97 <qkrwngud825@gmail.com>
2015-05-16 15:23:21 +0900

    Follow Android 5.0+ system app library style #2
    This is required for the system to correctly recognize webview.apk
    
    Relative symlink is done for not breaking build.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- chromium/Android.mk
+++ chromium/Android.mk
@@ -31,12 +31,20 @@ LOCAL_CERTIFICATE := platform
 
 ifeq ($(TARGET_IS_64_BIT),true)
 TARGET_ARCH_ABI := arm64-v8a
+TARGET_LIB_DIR := lib64
+TARGET_LIB_ARM_DIR := arm64
 else
 TARGET_ARCH_ABI := armeabi-v7a
+TARGET_LIB_DIR := lib
+TARGET_LIB_ARM_DIR := arm
 endif
 
 $(shell mkdir -p $(TARGET_OUT_SHARED_LIBRARIES))
 $(shell cp $(LOCAL_PATH)/prebuilt/$(TARGET_ARCH_ABI)/libwebviewchromium.so $(TARGET_OUT_SHARED_LIBRARIES))
+
+$(shell mkdir -p $(TARGET_OUT_APPS)/webview/lib/$(TARGET_LIB_ARM_DIR))
+$(shell ln -sf ../../../../$(TARGET_LIB_DIR)/libwebviewchromium.so $(TARGET_OUT_APPS)/webview/lib/$(TARGET_LIB_ARM_DIR)/libwebviewchromium.so)
+ALL_DEFAULT_INSTALLED_MODULES += $(TARGET_OUT_APPS)/webview/lib/$(TARGET_LIB_ARM_DIR)/libwebviewchromium.so
 
 include $(BUILD_PREBUILT)
 
